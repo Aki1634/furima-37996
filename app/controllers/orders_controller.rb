b0fce3172408507f534180521d6140b0b81e282a
Aki1634 <tanimizu3316@outlook.jp>
2022-07-08 08:37:47 +0900

    購入画面

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,42 @@
+class OrdersController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_item, only: [:index, :create]
+  
+    def index
+      if user_signed_in? && current_user.id != @product.user_id && @product.order == nil
+        @product_order = ProductOrder.new
+      else
+        redirect_to root_path
+      end
+    end
+  
+    def create
+      @product_order = ProductOrder.new(order_params)
+      if @product_order.valid?
+        pay_item
+        @product_order.save
+        return redirect_to root_path
+      else
+        render action: :index
+      end
+    end
+  
+    private
+  
+    def set_item
+      @product = Product.find(params[:product_id])
+    end
+  
+    def order_params
+      params.require(:product_order).permit(:postal_code, :prefecture_id, :city_name, :block_name, :building_name, :phone_number).merge(token: params[:token], product_id: params[:product_id], user_id: current_user.id )
+    end
+  
+    def pay_item
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+        Payjp::Charge.create(
+          amount: @product.price,  
+          card: order_params[:token],    
+          currency: 'jpy'                 
+        )
+      end
+end
---
+++ spec/models/product_oder_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+RSpec.describe ProductOrder, type: :model do
+  describe '購入者情報の保存' do
+    before do
+      @product_order = FactoryBot.build(:product_order)
+    end
+
+      it '全ての項目が入力されていれば購入ができる' do
+        expect(@product_order).to be_valid
+      end
+      it 'token(クレジットカード情報)が空だと購入ができない' do
+        @product_order.token = nil
+        @product_order.valid?
+        expect(@product_order.errors.full_messages).to include("Token can't be blank")
+      end
+      it '郵便番号が空だと購入ができない' do
+        @product_order.postal_code = ""
+        @product_order.valid?
+        expect(@product_order.errors.messages).to include({:postal_code => ["can't be blank", "is invalid", "is the wrong length (should be 8 characters)"]})
+      end
+      it '郵便番号にハイフンがないと登録できない' do
+        @product_order.postal_code = "12345678"
+        @productorder.valid?
+        expect(@product_order.errors.full_messages).to include("Postal code is invalid")
+      end
+      it '郵便番号が8桁でないと購入できない' do
+        @product_order.postal_code = "123-458"
+        @product_order.valid?
+        expect(@product_order.errors.full_messages).to include("Postal code is invalid", "Postal code is the wrong length (should be 8 characters)")
+      end
+      it 'prefecture_idが空だと購入できない' do
+        @product_order.prefecture_id = nil
+        @product_order.valid?
+        expect(@product_order.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it 'city_nameが空だと購入できない' do
+        @product_order.city_name = ""
+        @product_order.valid?
+        expect(@product_order.errors.full_messages).to include("City name can't be blank")
+      end
+      it 'block_nameが空だと購入できない' do
+        @product_order.block_name = ""
+        @product_order.valid?
+        expect(@product_order.errors.full_messages).to include("Block name can't be blank")
+      end
+      it 'phone_numberが空だと購入できない' do
+        @product_order.phone_number = ""
+        @product_order.valid?
+        expect(@product_order.errors.full_messages).to include("Phone number can't be blank", "Phone number is invalid")
+      end
+      it 'phone_numberが11桁でなければ購入できない' do
+        @product_order.phone_number = "080123456789"
+        @product_order.valid?
+        expect(@product_order.errors.full_messages).to include("Phone number is invalid")
+      end
+  end
+end
