fb81664cf1c1ea8102992c69a7d2fbf3d9225047
Aki1634 <tanimizu3316@outlook.jp>
2022-07-22 15:04:56 +0900

    修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_733dd0cc124b975289e1ef49");
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
