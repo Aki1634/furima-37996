6bc5ccea92586abd785fb48bf7a4f1c522180d7c
Aki1634 <tanimizu3316@outlook.jp>
2022-07-27 15:05:37 +0900

    修正

--- spec/models/product_oder_spec.rb
+++ spec/models/product_oder_spec.rb
@@ -8,6 +8,10 @@ RSpec.describe ProductOrder, type: :model do
       it '全ての項目が入力されていれば購入ができる' do
         expect(@product_order).to be_valid
       end
+      it '建物名が空でも購入できる' do
+        @product_order.building_name = ""
+        expect(@product_order).to be_valid
+      end
     end
     context '商品を購入できない場合' do
       it '郵便番号が空だと購入ができない' do
@@ -50,10 +54,25 @@ RSpec.describe ProductOrder, type: :model do
         @product_order.valid?
         expect(@product_order.errors.full_messages).to include("Phone number is invalid")
       end
+      it '電話番号に半角数字以外が含まれている場合は購入できない' do
+        @product_order.phone_number = "080123456aa"
+        @product_order.valid?
+        expect(@product_order.errors.full_messages).to include("Phone number is invalid")
+      end
       it 'トークンが空だと保存できないこと' do
         @product_order.token = nil
         @product_order.valid?
         expect(@product_order.errors.full_messages).to include("Token can't be blank")
+      end
+      it 'userが紐付いていなければ購入できない' do
+        @product_order.user_id = "2"
+        @product_order.valid?
+        expect(@product_order.errors.full_messages).to include()
+      end
+      it 'product_idが紐付いていなければ購入できない' do
+        @product_order.product_id = "6"
+        @product_order.valid?
+        expect(@product_order.errors.full_messages).to include()
       end
   end
 end
