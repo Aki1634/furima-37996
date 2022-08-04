1da0214b2b3212d789cd09c7611e9114007c13c4
Aki1634 <tanimizu3316@outlook.jp>
2022-08-04 11:55:27 +0900

    Update tax.js

--- app/javascript/tax.js
+++ app/javascript/tax.js
@@ -1,12 +1,14 @@
-window.addEventListener('load', () => {
+window.addEventListener("load",function(){
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
-  const inputValue = priceInput.value;
-  const addTaxDom = document.getElementById("add-tax-price");
-  const ProfitDom = document.getElementById("profit");
-  // 入力した金額をもとに販売手数料を計算する処理
-  addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
-  // 出品価格から販売手数料を引く処理
-  ProfitDom.innerHTML = inputValue - addTaxDom.innerHTML;
+    const inputValue = priceInput.value;
+
+    
+
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(priceInput.value * 0.1 );
+
+    const addPofitDom = document.getElementById("profit");
+    addPofitDom.innerHTML = Math.floor(priceInput.value - Math.round(priceInput.value * 0.1 ))
+  
   })
-});
